430a719560e15886fbfb851e0b06237eccc0adfd
abanoubmounir <abanoubmounir07@gmail.com>
2023-11-28 12:21:52 +0200

    edit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Dynamic Theme
+
+Theme
+
+#### License
+
+MIT
---
+++ theme/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Dynamic Theme",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Dynamic Theme")
+		}
+	]
